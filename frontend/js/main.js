5bb799bc5b71137479b483738f2592ac7c956f64
Oleg Serdyuk <oserdyuk@aligngeneral.com>
2022-06-21 17:53:02 -0700

    Updates to structure, created main.js file

---
+++ frontend/js/main.js
@@ -0,0 +1,14 @@
+
+// this funtion is responsible for counting how many times your resume was viewed
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return response.json()
+    }).then(response =>{
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    })
+}
